08a0238b2a33e302655a657e65adc7edcc2d7f2d
Videogameboy <hello@cheapmail.cc.cd>
2026-07-04 20:07:04 +0800

    new file:   C++/2026.7.4/A.cpp

---
+++ C++/2026.7.4/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int maxn=1e3+5;
+signed main()
+{
+    double a,b,c;
+    cin>>a>>b;
+    c=b*2/3;
+    if(a>c) cout<<"Yes";
+    else cout<<"No";
+    return 0;
+
+}
