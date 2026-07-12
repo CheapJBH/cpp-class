9961f07c4e2d192c75c2df8cfe02aa3ff3f6ccb8
Videogameboy <hello@cheapmail.cc.cd>
2026-07-12 15:30:07 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ C++/2026.7.12/test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*====================*/
+#define endl '\n'
+/*====================*/
+using lnt = long long;
+/*====================*/
+void Solve(void)
+{
+    
+}
+/*====================*/
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("IN.txt", "r+", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1; //cin >> T;
+    while (T--)Solve();
+    return 0;
+}
