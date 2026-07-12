b35a762b28af153d97700779f25752d8ac162d8b
Videogameboy <hello@cheapmail.cc.cd>
2026-07-12 15:33:25 +0800

    Please enter the commit message for your changes. Lines starting

--- C++/2026.7.12/test.cpp
+++ C++/2026.7.12/test.cpp
@@ -7,7 +7,47 @@ using lnt = long long;
 /*====================*/
 void Solve(void)
 {
-    
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    map<int,int>mp;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            mp[j]++;
+            cout<<mp[j]<<" ";
+        }
+        cout<<endl;
+    }
+    int k=1;
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+    int p=1; 
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<p++<<" ";
+        }
+        cout<<endl;
+    }
 }
 /*====================*/
 int main()
